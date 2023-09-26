a8c7ae6b7f86dfd7a2c34a9b7fd66a49e0678c1e
Marina Merzlyakova <marina.merzlyakova1009@yandex.ru>
2023-09-26 22:21:33 +0300

    Реализовала авторизацию, добавила логгеры запросов и ошибок

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
+const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000 } = process.env;
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 app.use(express.json());
@@ -26,6 +28,9 @@ app.use(cors({
 }));
 
 app.use(errors());
+app.use(errorHandler);
+app.use(requestLogger);
+app.use(errorLogger);
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,12 +4,14 @@ const userRoutes = require('./users');
 const { validationLogin, validationCreateUser } = require('../middlewares/validation');
 const { createUser, login, logout } = require('../controllers/users');
 const NotFoundError = require('../errors/NotFoundError');
+const auth = require('../middlewares/auth');
 
 router.post('/signin', validationLogin, login);
 router.post('/signup', validationCreateUser, createUser);
 router.use('/movies', movieRoutes);
 router.use('/users', userRoutes);
 router.post('/signout', logout);
+router.use(auth);
 
 router.use('*', () => {
   throw new NotFoundError('Страница не существует');
