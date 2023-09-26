1a406711e812e33fc123ced3032ff73d2ecaf8c1
Marina Merzlyakova <marina.merzlyakova1009@yandex.ru>
2023-09-26 18:00:54 +0300

    Создала модуль проверки

---
+++ backend/middlewares/validation.js
@@ -0,0 +1,63 @@
+const { celebrate, Joi } = require('celebrate');
+
+const urlRegex = /^(https?:\/\/)(www\.)?([a-z1-9-]{2,}\.)+[a-z]{2,}\/?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]*/i;
+
+const validationCreateUser = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+  }),
+});
+
+const validationLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validationUpdateProfile = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+});
+
+const validationUsersId = celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().required().length(24),
+  }),
+});
+
+const validationMovieId = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().hex().required().length(24),
+  }),
+});
+
+const validationCreateMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(urlRegex),
+    trailerLink: Joi.string().required().pattern(urlRegex),
+    thumbnail: Joi.string().required().pattern(urlRegex),
+    owner: Joi.string().required(),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  validationLogin,
+  validationCreateUser,
+  validationUpdateProfile,
+  validationUsersId,
+  validationMovieId,
+  validationCreateMovie,
+};
